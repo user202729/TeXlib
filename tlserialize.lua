754a179cf027d2d902e8ca7e4ef3e4599ba1ff40
user202729 <25191436+user202729@users.noreply.github.com>
2022-08-12 07:39:09 +0700

    tlserialize: shorten generated output in Lua implementation

--- tlserialize.lua
+++ tlserialize.lua
@@ -19,14 +19,49 @@ local finale=[[}\expandafter}\T%]]
 
 local alt_finale=[[}\expandafter}\csname __tlser_result:n\expandafter\endcsname\expandafter{\T}%]]
 
+-- use test_tlserialize_generate_table.tex to generate this table...
+
+local safecat, cssafe={}, {}
+safecat[33]=12 safecat[34]=12 safecat[36]=3 safecat[38]=4 safecat[39]=12 safecat[40]=12 safecat[41]=12 safecat[42]=12 safecat[43]=12 safecat[44]=12 safecat[45]=12 safecat[46]=12 safecat[47]=12 safecat[48]=12 safecat[49]=12 safecat[50]=12 safecat[51]=12 safecat[52]=12 safecat[53]=12 safecat[54]=12 safecat[55]=12 safecat[56]=12 safecat[57]=12 safecat[59]=12 safecat[60]=12 safecat[61]=12 safecat[62]=12 safecat[63]=12 safecat[65]=11 safecat[66]=11 safecat[67]=11 safecat[68]=11 safecat[69]=11 safecat[70]=11 safecat[71]=11 safecat[72]=11 safecat[73]=11 safecat[74]=11 safecat[75]=11 safecat[76]=11 safecat[77]=11 safecat[78]=11 safecat[79]=11 safecat[80]=11 safecat[81]=11 safecat[82]=11 safecat[83]=11 safecat[84]=11 safecat[85]=11 safecat[86]=11 safecat[87]=11 safecat[88]=11 safecat[89]=11 safecat[90]=11 safecat[91]=12 safecat[93]=12 safecat[96]=12 safecat[97]=11 safecat[98]=11 safecat[99]=11 safecat[100]=11 safecat[101]=11 safecat[102]=11 safecat[103]=11 safecat[104]=11 safecat[105]=11 safecat[106]=11 safecat[107]=11 safecat[108]=11 safecat[109]=11 safecat[110]=11 safecat[111]=11 safecat[112]=11 safecat[113]=11 safecat[114]=11 safecat[115]=11 safecat[116]=11 safecat[117]=11 safecat[118]=11 safecat[119]=11 safecat[120]=11 safecat[121]=11 safecat[122]=11 safecat[124]=12 cssafe[95]=true cssafe[58]=true cssafe[64]=true cssafe[35]=true
+
+for k, _ in pairs(safecat) do cssafe[k]=true end
+
+
 function tlserialize_unchecked(tl)
 	local result={preamble}
-	for _, token_obj in ipairs(tl) do
+
+
+	-- match the braces, find out which brace pairs can be safely included
+	local left_stack={}  -- list of indices of pending {s
+	local brace_safe={}
+	for index, token_obj in ipairs(tl) do
+		if L.is_bgroup(token_obj) then
+			if L.get_charcode(token_obj)==string.byte "{" then
+				left_stack[#left_stack+1]=index
+			else
+				left_stack[#left_stack+1]=-1
+			end
+		elseif L.is_egroup(token_obj) then
+			assert(#left_stack>0, "weird, input tl is unbalanced")
+			local left_index=left_stack[#left_stack]
+			left_stack[#left_stack]=nil
+			if left_index>=0 and L.get_charcode(token_obj)==string.byte "}" then
+				brace_safe[left_index]=true
+				brace_safe[index]=true
+			end
+		end
+	end
+	assert(#left_stack==0, "weird, input tl is unbalanced")
+
+
+	for index, token_obj in ipairs(tl) do
 		local cur
 		if L.is_explicit_character(token_obj) then
 			local cat=L.get_catcode(token_obj)
 			local char=L.get_charcode(token_obj)
-			if cat==6 then
+			if brace_safe[index] or cat==safecat[char] then
+				cur=utf8.char(char)
+			elseif cat==6 then
 				cur='\\P' .. char .. '.'
 			elseif cat==10 then
 				if char==32 then
@@ -47,7 +82,11 @@ function tlserialize_unchecked(tl)
 			else
 				result[#result+1]='\\S{'
 				for _, c in utf8.codes(token_obj.csname) do
-					result[#result+1]='\\O' .. c .. '.'
+					if cssafe[c] then
+						result[#result+1]=utf8.char(c)
+					else
+						result[#result+1]='\\O' .. c .. '.'
+					end
 				end
 				cur='}'
 			end
