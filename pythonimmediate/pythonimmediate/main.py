bd54f104b7466e8ca0ab392d4eec7ac1b359d605
user202729 <25191436+user202729@users.noreply.github.com>
2022-12-24 21:34:16 +0700

    Redirection in main.py to avoid Python bug when package in zipfile

---
+++ pythonimmediate/pythonimmediate/main.py
@@ -0,0 +1,4 @@
+from pythonimmediate import textopy
+
+# allow the package to get its own sourcecode.
+# without this indirection it would not be possible, see https://github.com/python/cpython/issues/86291
