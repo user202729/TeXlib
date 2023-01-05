d338c7c10463bc5217ebebfcd0a5f40a46fef12f
user202729 <25191436+user202729@users.noreply.github.com>
2023-01-05 11:05:25 +0700

    remove pythonimmediate directory

--- pythonimmediate/pythonimmediate/__init__.py
+++
@@ -1,1 +0,0 @@
-from .engine import default_engine
--- pythonimmediate/pythonimmediate/main.py
+++
@@ -1,7 +0,0 @@
-from pythonimmediate import textopy
-
-if __name__ == "__main__":
-	textopy.parent_process_main()
-
-# allow the package to get its own sourcecode.
-# without this indirection it would not be possible, see https://github.com/python/cpython/issues/86291
