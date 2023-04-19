9598aa22e690f174f146f2f0840415cda2e05041
user202729 <25191436+user202729@users.noreply.github.com>
2023-04-19 09:36:23 +0700

    Replace joblib with shelve

--- typstmathinput.py
+++ typstmathinput.py
@@ -1,13 +1,14 @@
-import joblib  # type: ignore
 import tempfile
 from pathlib import Path
 import subprocess
 import shutil
 import hashlib
 import re
+import typing
 from typing import Dict, Union
 from pythonimmediate import*
 from pythonimmediate.engine import default_engine
+import shelve
 
 tmpdir=Path(tempfile.gettempdir())/".typstmathinput-tmp"
 tmpdir.mkdir(exist_ok=True)
@@ -15,10 +16,21 @@ template_file=Path(__file__).parent/"typstmathinput-template.typ"
 shutil.copy(template_file, tmpdir)
 template_hash=hashlib.sha256((tmpdir/template_file.name).read_bytes()).digest()
 
-@joblib.Memory(location=tmpdir/"cache", bytes_limit=2**20, verbose=0).cache
-def typst_formula_to_tex(s: str, extra_preamble: str, template_hash: bytes)->str:
+cache=shelve.open(str(tmpdir/"cache"))
+template_hash_cache_key="_template_hash_"
+if cache.get(template_hash_cache_key)!=template_hash:
+	cache.clear()
+	cache[template_hash_cache_key]=template_hash
+
+def input_hash(s: str, extra_preamble: str)->str:
+	return hashlib.sha256((str(len(s)) + "|" + s + extra_preamble).encode('u8')).hexdigest()
+
+def typst_formula_to_tex(s: str, extra_preamble: str)->str:
+	hash_=input_hash(s, extra_preamble)
+	result: str=typing.cast(str, cache.get(hash_))
+	if result: return result
+
 	# s must not start or end with "$", but the return value will be wrapped in $...$ or similar
-	# template_hash is not directly used but this is needed to invalidate joblib cache
 	with tempfile.NamedTemporaryFile(dir=tmpdir, prefix="", suffix=".typ", delete=False, mode="w") as f:
 		n=Path(f.name)
 		f.write(r"""
@@ -39,8 +51,10 @@ def typst_formula_to_tex(s: str, extra_preamble: str, template_hash: bytes)->str
 		raise ValueError(f"Formula ${s}$ is invalid -- Typst finishes but does not provide PDF")
 	try: subprocess.run(["pdftotext", n.with_suffix(".pdf")], cwd=tmpdir, check=True)
 	finally: n.with_suffix(".pdf").unlink(missing_ok=True)
-	try: result: str=n.with_suffix(".txt").read_text().replace("\n", "").replace("\x0c", "")
+	try: result=n.with_suffix(".txt").read_text().replace("\n", "").replace("\x0c", "")
 	finally: n.with_suffix(".txt").unlink(missing_ok=True)
+
+	cache[hash_]=result
 	return result
 
 
@@ -68,7 +82,6 @@ def handle_formula(engine: "Engine")->None:
 	execute(typst_formula_to_tex(
 		s,
 		var["typstmathinputextrapreamble"],
-		template_hash,
 		)+"%")
 
 	finish_listen()
